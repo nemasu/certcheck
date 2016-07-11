8f1863f191ce6a608e636a74053ada0b3473de26
Nathan Torchia <nemasu@gmail.com>
2016-07-11 08:29:48 +0000

    pkcs7 support

---
+++ src/main/java/org/nooplinux/certcheck/exception/CertificateValidatorException.java
@@ -0,0 +1,18 @@
+package org.nooplinux.certcheck.exception;
+
+public class CertificateValidatorException extends RuntimeException {
+
+    private static final long serialVersionUID = -4478110368747577503L;
+
+    public CertificateValidatorException( Exception exception ) {
+        super( exception );
+    }
+
+    public CertificateValidatorException( String msg, Throwable e ) {
+        super( msg, e );
+    }
+
+    public CertificateValidatorException( String message ) {
+        super( message );
+    }
+}
